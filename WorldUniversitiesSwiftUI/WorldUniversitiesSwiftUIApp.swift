7a2ba0bd50b7d9af291987c26c488786b2fa04b5
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-07-23 17:27:48 +0500

    Initial Commit

---
+++ WorldUniversitiesSwiftUI/WorldUniversitiesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldUniversitiesSwiftUIApp.swift
+//  WorldUniversitiesSwiftUI
+//
+//  Created by Томирис Рахымжан on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WorldUniversitiesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
